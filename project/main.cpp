075de4efeea52cd3623d4f57a8e5e36feb3dd2b4
Karandeep Singh <karandeep041998@gmail.com>
2020-03-29 17:39:42 +0530

    first commit

---
+++ project/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   /* string name;
+    double weight,height,bmi;
+    cout<<"Welcome! to MCDM diet recommendation system";
+    cout<<"\nEnter your Name:";
+    cin>>name;
+    cout<<"\nEnter your weight in Kilograms: ";
+    cin>>weight;
+    cout<<"\nEnter you height in meters: ";
+    cin>>height;
+    bmi=weight/(height*height);
+    cout<<name<<", your Body Mass Index is: "<<bmi;*/
+    ofstream fout;
+    fout.open("C:\Users\s\Downloads\Mini Project Dataset.xlsx",ios::app);
+    return 0;
+}
